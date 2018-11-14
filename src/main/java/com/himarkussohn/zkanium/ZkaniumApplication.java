40f1a6f9925cc8d14227093c70589cdc777a4860
himarkussohn <jmarkus.dev@gmail.com>
2018-11-14 22:24:23 +0200

    initial commit

---
+++ src/main/java/com/himarkussohn/zkanium/ZkaniumApplication.java
@@ -0,0 +1,12 @@
+package com.himarkussohn.zkanium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkaniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkaniumApplication.class, args);
+	}
+}
